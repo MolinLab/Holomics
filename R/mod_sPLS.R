535d95be83511a88af8487a85edd4a57ca5c0d12
Munk Katharina <katharina.munk.fl@ait.ac.at>
2022-12-27 12:41:18 +0100

    Warning pop-up shows up when all sPLS Q2 scores below 0

--- R/mod_sPLS.R
+++ R/mod_sPLS.R
@@ -378,6 +378,17 @@ tune_values <- function(dataSelection, result, tunedVals, input, output){
     output$Tuned.ncomp.download <- getDownloadHandler("PLS_Q2_Components_Plot.png", function(){plot(tune.spls, criterion = 'Q2.total')})
     output$Tuned.keepX.download <- getDownloadHandler("PLS_keepX_Plot.png", function(){plot(tune.X)})
     output$Tuned.keepY.download <- getDownloadHandler("PLS_keepY_Plot.png", function(){plot(tune.Y)})
+    
+    if(all(tune.spls$measures$Q2.total$summary$mean < 0)){
+      shinyalert::shinyalert("Warning!", HTML("Unfortunately, the Q2 score for all the components is below 0, 
+                                    which could be the result of a too low number of samples or that X does not explain Y 
+                                    (for more information see: 
+                                    <a class='mixOmics-link' href='https://mixomics-users.discourse.group/t/q2-total-negative-in-perf-pls/138/6' 
+                                    target='_blank'>Q2.total negative in perf.pls (Lê Cao, 2020)</a>) </br>.
+                                    In general, please consider using the untuned version."), 
+                             type = "warning", html = T
+      )
+    }
   }
 }
 
