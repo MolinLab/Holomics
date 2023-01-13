59b5f1b1110d28901f3120fb3e0ac9f8915cc2db
Munk Katharina <katharina.munk.fl@ait.ac.at>
2023-01-13 10:32:39 +0100

    Increased the possible file upload size

--- app.R
+++ app.R
@@ -4,4 +4,5 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
+options(shiny.maxRequestSize = 50*1024^2)
 Holomics::run_app() # add parameters here (if any)
